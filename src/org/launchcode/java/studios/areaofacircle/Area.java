6709a1d52a3c849e859d17f553f855c7799558c8
cjconnor18 <cjallen1988@gmail.com>
2020-01-06 20:01:45 -0600

    bonus missions

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-        System.out.println("Please enter radius of the circle:");
-        double radius=input.nextDouble();
+        double radius=0.0;
+        boolean error=false;
+        do {
+            System.out.println("Please enter radius of the circle:");
+            if(input.hasNextDouble()) {
+                radius = input.nextDouble();
+            } else {
+                System.out.println("Error: Numbers only");
+                error=true;
+            }
+        } while (radius<0);
+        if(!error){
+            Double area=Circle.getArea(radius);
+            System.out.println("The area of a circle of radius "+ radius + " is: " + area);
+        }
         input.close();
-
-        Double area=Circle.getArea(radius);
-        System.out.println("The area of a circle of radius "+ radius + " is: " + area);
     }
 }
